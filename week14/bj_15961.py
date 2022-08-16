5c5d521537f959c10b8ae2436583ab37e213668c
semi-cloud <sksmsrkdtpal@naver.com>
2022-08-17 00:59:39 +0900

    refactor: 회전 초밥 15961

--- week14/bj_15961.py
+++ week14/bj_15961.py
@@ -1,34 +1,29 @@
 # 손님이 먹을 수 있는 초밥 가짓수의 "최댓값"을 구하기
-
 from collections import defaultdict
 
 N, d, k, c = map(int, input().split())
-foods = [int(input()) for _ in range(N)]
-foods.extend(foods)       # 원형 배열 생성
-l, r, result = 0, 0, 0
-dict_ = defaultdict(int)
-dict_[c] = 1  # 접시를 k개 연속해서 먹으면 쿠폰 무조건 발급
+arr = [int(input()) for _ in range(N)]
+canEat = defaultdict(int)
+cnt, canEat[c] = 1, 1      # cnt : 먹을 수 있는 초밥의 종류의 개수
 
-# 1. k개의 접시를 먹고 딕셔너리에 저장
-while r < k:
-    dict_[foods[r]] += 1
-    r += 1
+for i in range(k):         # k개의 접시를 먹고 딕셔너리에 저장
+    if canEat[arr[i]] == 0:
+        cnt += 1
+    canEat[arr[i]] += 1
 
-# 2. 슬라이딩 윈도우 적용
-while r < len(foods):
-    result = max(result, len(dict_))   # 딕셔너리의 길이 = 먹을 수 있는 초밥 개수
+answer = cnt
+for left in range(N):
+    right = (left + k) % N     # 원형 배열 인덱스
+    canEat[arr[left]] -= 1     # 왼쪽 포인터
 
-    # 슬라이딩 1 : 맨 왼쪽 초밥 제거
-    dict_[foods[l]] -= 1
-    if dict_[foods[l]] == 0:   # 0이 되면 딕셔너리에서 제거
-        del dict_[foods[l]]
+    if canEat[arr[left]] == 0:   # 아예 해당 원소가 없는 경우
+        cnt -= 1
+    if canEat[arr[right]] == 0:   # 새로운 원소인 경우
+        cnt += 1
+    canEat[arr[right]] += 1     # 오른쪽 포인터 이동
 
-    # 슬라이딩 2 : 오른쪽 초밥 추가
-    dict_[foods[r]] += 1
-    l += 1
-    r += 1
-print(result)
-
+    answer = max(answer, cnt)   # 먹을 수 있는 종료 개수의 최댓값 저장
+print(answer)
 
 
 
