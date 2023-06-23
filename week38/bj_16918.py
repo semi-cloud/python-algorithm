218a114fac395a038656140730e8b15a695931ca
semi-cloud <sksmsrkdtpal@naver.com>
2023-06-23 22:35:54 +0900

    refactor: 시간 줄이기 리팩토링

--- week38/bj_16918.py
+++ week38/bj_16918.py
@@ -1,39 +1,31 @@
 def set_bomb(cur_time):  # 1초가 더 지나면 폭탄이 없는 모든 칸에 폭탄 설치
     for i in range(R):
         for j in range(C):
-            if arr[i][j][0] == '.':
-                arr[i][j] = ['O', cur_time + 3]
+            if arr[i][j] == -1:
+                arr[i][j] = cur_time + 3
 
 dx, dy = [0, 0, 1, -1], [1, -1, 0, 0]
 def explode_bomb(cur_time):  # 1초가 더 지나면 폭탄이 폭발 하면서 상하좌우 연쇄 폭발
     bomb_list = []
     for i in range(R):
         for j in range(C):
-            if arr[i][j][0] == '.' or arr[i][j][1] != cur_time: continue
-
-            arr[i][j] = ['.', 0]
-            for k in range(4):   # 폭발 시간이 된 폭탄만 검사
-                nx, ny = i + dx[k], j + dy[k]
-                if nx < 0 or nx >= R or ny < 0 or ny >= C: continue
-                bomb_list.append([nx, ny])  # 제거할 인접한 폭탄 리스트 저장(기존에 존재하던 폭탄은 유지 해야 하므로 바로 제거 X)
+            if arr[i][j] == -1 or arr[i][j] > cur_time: continue
+            bomb_list.append((i, j))
 
     for x, y in bomb_list:
-        arr[x][y] = ['.', 0]
+        arr[x][y] = -1
+        for k in range(4):  # 폭발 시간이 된 폭탄만 검사
+            nx, ny = x + dx[k], y + dy[k]
+            if nx < 0 or nx >= R or ny < 0 or ny >= C: continue
+            arr[nx][ny] = -1       # 제거할 인접한 폭탄 리스트 저장(기존에 존재하던 폭탄은 유지 해야 하므로 바로 제거 X)
 
 import sys
 input = sys.stdin.readline
 
 R, C, N = map(int, input().split(" "))  # N초
-arr = [[list() for _ in range(C)] for _ in range(R)]
+temp = [input() for _ in range(R)]
 time = 0
-
-for i in range(R):
-    temp = input()
-    for j in range(C):
-        if temp[j] == 'O':
-            arr[i][j] = [temp[j], time + 3]  # [폭탄 여부(원소), 폭발 예정 시간]
-        else:
-            arr[i][j] = [temp[j], 0]
+arr = [[time + 3 if temp[i][j] == 'O' else -1 for j in range(C)] for i in range(R)]
 
 time += 1   # 1초 동안 아무것도 안함
 while True:     # 폭탄은 설치 후 3초 뒤에 폭발
@@ -46,5 +38,8 @@ while True:     # 폭탄은 설치 후 3초 뒤에 폭발
 
 for i in range(R):
     for j in range(C):
-        print(arr[i][j][0], end="")
+        if arr[i][j] == -1:
+            print(".", end="")
+        else:
+            print("O", end="")
     print()
